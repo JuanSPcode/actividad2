a984b0c565c99013e28bd468d1c3c04f426f9c24
Juan Sandoval <js3729395@gmail.com>
2025-09-25 12:56:59 -0600

    fin

--- app/controllers/HomeController.php
+++ app/controllers/HomeController.php
@@ -29,6 +29,53 @@ class HomeController{
         ]);
     }
 
+    public function contactoCheck(){
+        // validando datos con expresiones regulares
+        $nombre = $_POST['name'] ?? "no agregado";
+        $tel = $_POST['telefono'] ?? "no agregado";
+        $email = $_POST['email'] ?? "no agregado";
+        $dui = $_POST['dui'] ?? "no agregado";
+        // Procesar los datos (por ejemplo, guardarlos en una base de datos o enviarlos por correo electrónico)
+        // Validar el formato del correo electrónico
+        $nombrergx = "/^[a-zA-ZÀ-ÿ\s]{1,40}$/"; // Letras y espacios, pueden llevar acentos.
+        $telrgx = "/^\d{4}-\d{4}$/"; //
+        $emailrgx = "/^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/"; // Formato básico de correo electrónico
+        $duirgx = "/^\d{8}-\d{1}$/"; //
+        if (!preg_match($nombrergx, $nombre)) {
+            return $this->view("contacto", [
+                "title" => "Mi Sitio - contacto,
+                'error' => 'Nombre no válido. Solo se permiten letras y espacios.'"
+            ]) ;
+        }
+        if (!preg_match($telrgx, $tel)) {
+
+            return $this->view("contacto", [
+                "title" => "Mi Sitio - contacto",
+                'error' => 'Teléfono no válido. El formato correcto es ####-####.'
+            ])  ;
+        }
+        if (!preg_match($emailrgx, $email)) {
+
+            return $this->view("contacto", [
+                "title" => "Mi Sitio - contacto",
+                'error' => 'Correo electrónico no válido.'
+            ]);
+        }
+        if (!preg_match($duirgx, $dui)) {
+
+            return $this->view("contacto", [
+                "title" => "Mi Sitio - contacto",
+                'error' => 'DUI no válido. El formato correcto es ########-#.'
+            ]);
+        }
+
+
+
+        return $this->view("contacto", [
+            "title" => "Mi Sitio - contacto"
+        ]);
+    }
+
 
     public function view($vista, $data=[]){
         //require_once("../app/views/HomeView.php");
--- app/views/contacto.php
+++ app/views/contacto.php
@@ -30,9 +30,56 @@
   </div>
 </nav>
 
+<div style="width: 550px;" class="container-sm shadow text-center my-3 p-5">
+  <div>
+    <?php if (isset($error)): ?>
+      <div class="alert alert-danger" role="alert">
+        <?php echo htmlspecialchars($error); ?>
+      </div>
+    <?php endif; ?>
+  </div>
+    <h1 class="text-center">Contacto</h1>
+    <form action="" method="post">
+        <div class="mb-3 row">
+            <label for="name" class="col-sm-2 col-form-label">Nombre:</label>
+            <div class="col-sm-10">
+                <input type="text" class="form-control" id="name" name="name" required>
+            </div>
+        </div>
+        <div class="mb-3 row">
+            <label for="telefono" class="col-sm-2 col-form-label">Teléfono:</label>
+            <div class="col-sm-10">
+                <input type="text" class="form-control" id="telefono" name="telefono" required>
+            </div>
+        </div>
+        <div class="mb-3 row">
+            <label for="email" class="col-sm-2 col-form-label">Email:</label>
+            <div class="col-sm-10">
+                <input type="tex" class="form-control" id="email" name="email" required>
+            </div>
+        </div>
+        <div class="mb-3 row">
+            <label for="dui" class="col-sm-2 col-form-label">DUI:</label>
+            <div class="col-sm-10">
+                <input type="text" class="form-control" id="dui" name="dui" required>
+            </div>
+        </div>
 
+        <div class="row">
+            <div class="col-sm-10 offset-sm-2">
+                <button type="submit" class="btn btn-primary">Enviar</button>
+            </div>
+        </div>
+    </form>
+</div>
 
-
+    <div >
+      <?php if ($_SERVER["REQUEST_METHOD"] == "POST"): ?>
+        <pre>
+          <?php print_r($_POST); ?>
+        </pre>
+      <?php endif; ?>
+    </div>
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/js/bootstrap.bundle.min.js" integrity="sha384-FKyoEForCGlyvwx9Hj09JcYn3nv7wiPVlz7YYwJrWVcXK/BmnVDxM+D2scQbITxI" crossorigin="anonymous"></script>
 </body>
 </html>
